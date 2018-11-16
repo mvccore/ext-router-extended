75216e973fb83ccf59304466290bb2d802df531f
Tom Flídr <tomflidr@gmail.com>
2018-11-16 10:05:25 +0100

    comments grammar fixes

--- src/MvcCore/Ext/Routers/IExtended.php
+++ src/MvcCore/Ext/Routers/IExtended.php
@@ -43,9 +43,9 @@ interface IExtended
 	 * only by requesting different url with different site version prefix. If he does it
 	 * and this configuration is `TRUE`, he is redirected back to his remembered site 
 	 * version by session. 
-	 * But if you realy want to switch site version for your users, you need to add into 
+	 * But if you really want to switch site version for your users, you need to add into 
 	 * url special param to switch the version. But if you are creating url in controller 
-	 * or in template, it's added automaticly, when you put into second argument `$params` 
+	 * or in template, it's added automatically, when you put into second argument `$params` 
 	 * key with different site version:
 	 * `$this->Url('self', ['media_version' => 'mobile', 'lang'	=> 'de']);`.
 	 * @return bool
@@ -57,9 +57,9 @@ interface IExtended
 	 * only by requesting different url with different site version prefix. If he does it
 	 * and this configuration is `TRUE`, he is redirected back to his remembered site 
 	 * version by session. 
-	 * But if you realy want to switch site version for your users, you need to add into 
+	 * But if you really want to switch site version for your users, you need to add into 
 	 * url special param to switch the version. But if you are creating url in controller 
-	 * or in template, it's added automaticly, when you put into second argument `$params` 
+	 * or in template, it's added automatically, when you put into second argument `$params` 
 	 * key with different site version:
 	 * `$this->Url('self', ['media_version' => 'mobile', 'lang'	=> 'de']);`.
 	 * @param bool $stricModeBySession
@@ -70,15 +70,15 @@ interface IExtended
 	/**
 	 * Get session expiration in seconds to remember previously detected site version by user
 	 * agent or headers from previous requests. To not recognize site version by user agent 
-	 * or headers everytime, because it's time consuming. Default value is `0` - "until the  
+	 * or headers every time, because it's time consuming. Default value is `0` - "until the  
 	 * browser is closed". Session record is always used to compare, if user is requesting the  
 	 * same or different site version. If request by url is into the same site version, 
 	 * session record expiration is enlarged by this value. If request by url is into 
 	 * current session place, session different site version, then new different 
 	 * site version is stored in expiration is enlarged by this value and user is 
 	 * redirected to different place. But if router is configured into session strict mode, 
-	 * than to redirect user into new site version, there is necesary to add special 
-	 * url switch param (always automaticly added by `Url()` method). Because without it, 
+	 * than to redirect user into new site version, there is necessary to add special 
+	 * url switch param (always automatically added by `Url()` method). Because without it, 
 	 * user is redirected strictly back into the same version.
 	 * @return int
 	 */
@@ -87,15 +87,15 @@ interface IExtended
 	/**
 	 * Set session expiration in seconds to remember previously detected site version by user
 	 * agent or headers from previous requests. To not recognize site version by user agent 
-	 * or headers everytime, because it's time consuming. Default value is `0` - "until the  
+	 * or headers every time, because it's time consuming. Default value is `0` - "until the  
 	 * browser is closed". Session record is always used to compare, if user is requesting the  
 	 * same or different site version. If request by url is into the same site version, 
 	 * session record expiration is enlarged by this value. If request by url is into 
 	 * current session place, session different site version, then new different 
 	 * site version is stored in expiration is enlarged by this value and user is 
 	 * redirected to different place. But if router is configured into session strict mode, 
-	 * than to redirect user into new site version, there is necesary to add special 
-	 * url switch param (always automaticly added by `Url()` method). Because without it, 
+	 * than to redirect user into new site version, there is necessary to add special 
+	 * url switch param (always automatically added by `Url()` method). Because without it, 
 	 * user is redirected strictly back into the same version.
 	 * @param int $sessionExpirationSeconds
 	 * @return \MvcCore\IRouter|\MvcCore\Ext\Routers\IExtended
